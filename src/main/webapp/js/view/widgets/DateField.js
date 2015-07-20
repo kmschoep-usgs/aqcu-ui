5aa4a5e599ac1027304df4cdb0430b689c8a2c21
David Uselmann <duselmann@usgs.gov>
2015-07-20 18:05:26 -0500

    water year and last month change events

--- src/main/webapp/js/view/widgets/DateField.js
+++ src/main/webapp/js/view/widgets/DateField.js
@@ -18,7 +18,7 @@ AQCU.view.DateField = Backbone.View.extend({
 			{{/if}}\
 			{{#if includeLastMonths}}\
 			<div class='col-sm-3 col-lg-3'>\
-				<select class='form-control'>\
+				<select class='form-control vision_field_lastMonths_{{fieldName}}'>\
 					<option>Last 12 months</option>\
 					<option>Last 11 months</option>\
 					<option>Last 10 months</option>\
@@ -40,16 +40,16 @@ AQCU.view.DateField = Backbone.View.extend({
 					<label>Water Year:</label>\
 				</div>\
 				<div style='float:left;width:50%'>\
-			    	<input type='text' class='input-sm form-control vision_field input_wateryear_{{fieldName}}'/>\
+			    	<input type='text' class='input-sm form-control vision_field vision_field_waterYear_{{fieldName}}'/>\
 				</div>\
 			</div>\
 			{{/if}}\
 			<div class='col-sm-4 col-lg-4'>\
 				<div class='input-daterange input-group input_date_{{fieldName}}' >\
-				    <input type='text' class='input-sm form-control vision_field' name='start' />\
+				    <input type='text' class='input-sm form-control vision_field input_date_start_{{fieldName}}' name='start' />\
 				    {{#if isDateRange}}\
 				    <span class='input-group-addon'>to</span>\
-				    <input type='text' class='input-sm form-control vision_field' name='end' />\
+				    <input type='text' class='input-sm form-control vision_field input_date_end_{{fieldName}}' name='end' />\
 				    {{/if}}\
 				</div>\
 			</div>\
@@ -68,23 +68,59 @@ AQCU.view.DateField = Backbone.View.extend({
 		this.fieldConfig = this.options.fieldConfig;
 		this.renderTo    = this.options.renderTo;
 		this.select2     = this.options.select2;
-		this.startField  = ".vision_field_start_" + this.fieldConfig.fieldName;
-		this.endField    = ".vision_field_end_" + this.fieldConfig.fieldName;
-
-
-		// set default value - override available
-//		if ( ! this.select2.minimumInputLength) {
-//			this.select2.minimumInputLength = 3;
-//		}
+		this.startField  = ".input_date_start_" + this.fieldConfig.fieldName;
+		this.endField    = ".input_date_end_" + this.fieldConfig.fieldName;
 		
 		this.filterRequired = this.options.filterRequired;
 		
-//		this.events["change " + this.field] = this.updateSelectedOption;
-//		this.events["change " + this.selector] = this.setHiddenValue;
+		// TODO this could be a method
+		if (this.fieldConfig.includeLastMonths) {
+			this.lastMonths  = ".vision_field_lastMonths_" + this.fieldConfig.fieldName;
+			this.events["change " + this.lastMonths] = this.updateLastMonths;
+			this.fieldConfig.isDateRange = true;
+		}
+		if (this.fieldConfig.includeWaterYear) {
+			this.waterYear  = ".vision_field_waterYear_" + this.fieldConfig.fieldName;
+			this.events["change " + this.waterYear] = this.updateWaterYear;
+			this.fieldConfig.isDateRange = true;
+		}
 
 		Backbone.View.prototype.initialize.apply(this, arguments);
 		this.render();
 //		this.updateSelectedOption();
+	},
+	formatDate: function(date) {
+		return $.format.date(date, "MM/dd/yyyy");
+	},
+	updateLastMonths: function() {
+		var months = 12 - this.$(this.lastMonths).prop('selectedIndex');
+		
+		var date   = new Date();
+		var end    = this.formatDate(date);
+
+		date.setMonth( date.getMonth() - months );
+		var start  = this.formatDate(date);
+		
+		this.$(this.startField).val(start);
+		this.$(this.endField).val(end);
+	},
+	updateWaterYear: function() {
+		var year   = parseInt( this.$(this.waterYear).val() );
+		var date   = new Date();
+		
+		date.setDate(1);
+		date.setMonth(9); // Oct 
+		date.setYear(year-1);
+		var start  = this.formatDate(date);
+		
+		date.setDate(30);
+		date.setMonth(8);
+		date.setYear(year);
+		var end  = this.formatDate(date);
+		
+		this.$(this.startField).val(start);
+		this.$(this.endField).val(end);
+		
 	},
 	render: function() {
 		var newDom = this.template(this.fieldConfig); //new DOM elements created from templates
