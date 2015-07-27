01d6e3d6e90e6beacec585831a3813f20bafbf12
David Uselmann <duselmann@usgs.gov>
2015-07-27 10:47:51 -0500

    revert default date range, conditionally set end date if is range, code cleanup

--- src/main/webapp/js/view/widgets/DateField.js
+++ src/main/webapp/js/view/widgets/DateField.js
@@ -34,8 +34,8 @@ AQCU.view.DateField = Backbone.View.extend({
 					{{#if includeLastMonths}}\
 					<div class='col-sm-4 col-lg-4'>\
 						<select class='form-control vision_field_lastMonths_{{fieldName}}'>\
-							<option selected='selected'>Select Recent Months</option>\
-							<option>Last 12 months</option>\
+							<option value='none'>Select Recent Months</option>\
+							<option selected='true'>Last 12 months</option>\
 							<option>Last 11 months</option>\
 							<option>Last 10 months</option>\
 							<option>Last  9 months</option>\
@@ -71,21 +71,22 @@ AQCU.view.DateField = Backbone.View.extend({
 	 * Will create and render a text field using a fieldConfig options defined in NWIS Search Models.
 	 */
 	initialize: function() {
-		this.model       = this.options.model || this.options.searchModel; // stickit requires model not searchModel
 		this.fieldConfig = this.options.fieldConfig;
 		this.renderTo    = this.options.renderTo;
 		
+		// stickit requires model rather than searchModel which included for backward compatibility
+		this.model       = this.options.model              ?this.options.model              :this.options.searchModel;
 		this.waterYearMin= this.options.waterYearMin       ?this.options.waterYearMin       :1900;
 		this.startDate   = this.options.startDateFieldName ?this.options.startDateFieldName :"startDate";
 		this.endDate     = this.options.endDateFieldName   ?this.options.endDateFieldName   :"endDate";
-		this.dateFormat  = this.options.format             ?this.options.format             :"mm/dd/yyyy";
+		this.dateFormat  = this.options.format             ?this.options.format             :"yyyy-mm-dd";
 		
-		this.dateGroup   = ".input_date_" + this.fieldConfig.fieldName;
+		this.dateGroup   = ".input_date_"       + this.fieldConfig.fieldName;
 		this.startField  = ".input_date_start_" + this.fieldConfig.fieldName;
-		this.endField    = ".input_date_end_" + this.fieldConfig.fieldName;
-		this.hiddenField = ".vision_field_" + this.fieldConfig.fieldName;
+		this.endField    = ".input_date_end_"   + this.fieldConfig.fieldName;
+		this.hiddenField = ".vision_field_"     + this.fieldConfig.fieldName;
 		
-		this.bindings = {}; // TODO can binding be dynamic like this?
+		this.bindings = {}; // dynamic binding
 		this.bindings[this.startField] = this.startDate;
 		this.bindings[this.endField]   = this.endDate;
 		
@@ -110,20 +111,18 @@ AQCU.view.DateField = Backbone.View.extend({
 
 		Backbone.View.prototype.initialize.apply(this, arguments);
 		this.render();
-		this.stickit();
 		
 		if (this.fieldConfig.includeLastMonths) {
 			// TODO in order for this to work the change events above must be processed - for now call direct work around
 			//this.$(this.lastMonthsField).change();
-			var view = this;
-			setTimeout(function() {
-				view.updateLastMonths();
-			},500);
+			this.updateLastMonths();
 		}
 	},
 	setDates: function(start, end) {
 		this.$(this.startField).datepicker('setDate', start);
-		this.$(this.endField  ).datepicker('setDate',   end);
+		if (this.fieldConfig.isDateRange) {
+			this.$(this.endField  ).datepicker('setDate',   end);
+		}
 	},
 	getDates: function() {
 		var dates = [this.$(this.startField).val()];
@@ -139,32 +138,32 @@ AQCU.view.DateField = Backbone.View.extend({
 	updateLastMonths: function() {
 		var months = 13 - this.$(this.lastMonthsField).prop('selectedIndex');
 
-		var end    = new Date();
-		var end    = new Date(end.getYear()+1900, end.getMonth(), end.getDate());
-		var start  = new Date(end.getYear()+1900, end.getMonth(), end.getDate());
-		start.setMonth( start.getMonth() - months );
+		var current = new Date();
+		var start   = new Date(current.getYear()+1900, current.getMonth()-months, current.getDate());
+		this.setDates(start, current);
 		
-		this.setDates(start, end);
-		this.clearFields(this.lastMonthsField);
+		this.clearFields(this.lastMonthsField);  // clear date fields other than this one
 	},
 	updateWaterYear: function() {
+		// reset field to have no error status
 		this.$(this.waterYearGroup).removeClass("has-error");
 		
-		var year;
 		try {
-			year     = parseInt( this.$(this.waterYearField).val() );
-			var date = new Date();
+			var givenYear   = parseInt( this.$(this.waterYearField).val() );
+			var currentYear = new Date().getYear()+1900;
 			
-			if ( ! $.isNumeric(year) || year > date.getYear()+1900 || year < this.waterYearMin) {
-				throw new Error();
+			if ( ! $.isNumeric(givenYear) || givenYear > currentYear || givenYear < this.waterYearMin) {
+				throw new Error(); // trigger error status class
 			}
 			
-			var start = new Date(year-1, 9,  1);
-			var end   = new Date(year,   8, 30);
+			var start = new Date(givenYear-1, 9,  1);
+			var end   = new Date(givenYear,   8, 30);
+			this.setDates(start, end);
 			
-			this.setDates(start,end);
-			this.clearFields(this.waterYearField);
+			this.clearFields(this.waterYearField); // clear date fields other than this one
+			
 		} catch (e) {
+			// color the field with the error status class
 			this.$(this.waterYearGroup).addClass("has-error");
 		}
 	},
@@ -185,6 +184,7 @@ AQCU.view.DateField = Backbone.View.extend({
 		    .on("hide", function(e) {
 				view.clearFields();
 		    });
+		this.stickit();
 	},
 	clearFields: function(exclude) {
 		if (exclude !== this.waterYearField) {
