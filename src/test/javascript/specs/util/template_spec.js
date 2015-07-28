8618c3fe156dc60ac4432785256e328aa240aa68
dpattermann-usgs <dpattermann@usgs.gov>
2015-07-28 10:40:42 -0500

    Fixed merge of template_spec.js

--- src/test/javascript/specs/util/template_spec.js
+++ src/test/javascript/specs/util/template_spec.js
@@ -1,10 +1,6 @@
 describe("template.js", function() {
-<<<<<<< HEAD
 	it("has these dependencies", function() {
 	});
-
-=======
->>>>>>> 8fef2059ccdc1d5adfcfab3c6b02f98498d6d1d3
 	it("defines these API functions and fields", function() {
 		expect(AQCU.util.template).toBeDefined();
 		expect(AQCU.util.template.templateBasePath).toBeDefined();
@@ -14,10 +10,7 @@ describe("template.js", function() {
 
 describe("AQCU.util.template.getTemplate", function() {
 	it("it produces a Handlebars template of the returned data", function() {
-<<<<<<< HEAD
-		expect(AQCU.util.template.getTemplate("seleaaact-sites")).toBeDefined();
-=======
 		expect(AQCU.util.template.getTemplate("seleaaact-sites")).toBeDefined(); //TODO actually test the resolution of the promise
->>>>>>> 8fef2059ccdc1d5adfcfab3c6b02f98498d6d1d3
+
 	});
 });
