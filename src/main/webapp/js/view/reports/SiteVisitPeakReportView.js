1da9bd584fecf5405f8c9cde30b9f35b40b4b5c0
dpattermann-usgs <dpattermann@usgs.gov>
2015-09-15 11:17:59 -0500

    Removed the sublocation identifier from the advanced options UI

--- src/main/webapp/js/view/reports/SiteVisitPeakReportView.js
+++ src/main/webapp/js/view/reports/SiteVisitPeakReportView.js
@@ -1,16 +1,7 @@
 AQCU.view.SiteVisitPeakReportView = AQCU.view.BaseReportView.extend({
 	reportName: "Site Visit Peak",
-	reportType: "sitevisitpeak",
-	optionalRelatedTimeseriesConfig: [
-		//Also might be unnecessary. Unsure if sublocation is something you display or what?
-		{
-			requestId: "sublocation",
-			display: "Sublocation",
-			direction: "upchain",
-			publish: 'true',
-			computation: 'Unknown',
-			period: 'Unknown'
-		}]
+	reportType: "sitevisitpeak"
+
 });
 
 
