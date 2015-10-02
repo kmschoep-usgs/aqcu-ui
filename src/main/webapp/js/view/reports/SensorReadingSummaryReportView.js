b67ab6619eeedc48a0b9b79a60b3af1a30162069
dpattermann-usgs <dpattermann@usgs.gov>
2015-10-02 15:41:30 -0500

    Changed advanced options in SRS report view and modified SRS orchestrator and readings Handler to return only portions of Readings we want for SRS.

--- src/main/webapp/js/view/reports/SensorReadingSummaryReportView.js
+++ src/main/webapp/js/view/reports/SensorReadingSummaryReportView.js
@@ -1,22 +1,5 @@
 AQCU.view.SensorReadingSummaryReportView = AQCU.view.BaseReportView.extend({
 	reportName: "Sensor Reading Summary",
 	reportType: "sensorreadingsummary",
-	optionalRelatedTimeseriesConfig: [{
-			requestId: "upchainTimeseriesIdentifier",
-			display: "Gage Height",
-			parameter: "Discharge",
-			direction: "upchain",
-			publish: 'true',
-			computation: 'Unknown',
-			period: 'Unknown'
-		},{
-			requestId: "upchainTimeseriesIdentifier",
-			display: "Precipitation",
-			parameter: "Precipitation",
-			direction: "upchain",
-			publish: 'true',
-			computation: 'Unknown',
-			period: 'Unknown'
-		}]
 });
 
