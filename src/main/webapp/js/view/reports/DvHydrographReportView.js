df686b7ee182e918d844c61c91d1b1e551101908
dpattermann-usgs <dpattermann@usgs.gov>
2015-11-19 13:35:31 -0600

    Added javascript to dv/gw summary to add in three options for downchain stat derived dv

--- src/main/webapp/js/view/reports/DvHydrographReportView.js
+++ src/main/webapp/js/view/reports/DvHydrographReportView.js
@@ -1,45 +1,77 @@
 AQCU.view.DvHydrographReportView = AQCU.view.BaseReportView.extend({
 	reportName: "DV Hydrograph",
 	reportType: "dvhydrograph",
-	optionalRelatedTimeseriesConfig: [{			
+	optionalRelatedTimeseriesConfig: [{
 			requestId: "firstDownChainIdentifier",
 			display: "First Downchain Stat Derived Time Series",
 			direction: "downchain",
 			publish: 'true',
 			parameter: 'userSelectedParameter',
-			period: 'Daily'
-		},{
+			period: 'Daily',
+			dynamicParameter: 'true'
+		}, {
 			requestId: "secondDownChainIdentifier",
 			display: "Second Downchain Stat Derived Time Series",
 			direction: "downchain",
 			publish: 'true',
 			parameter: 'userSelectedParameter',
-			period: 'Daily'
-		},{
+			period: 'Daily',
+			dynamicParameter: 'true'
+		}, {
 			requestId: "thirdDownChainIdentifier",
 			display: "Third Downchain Stat Derived Time Series",
 			direction: "downchain",
 			publish: 'true',
 			parameter: 'userSelectedParameter',
-			period: 'Daily'
-		},{
+			period: 'Daily',
+			dynamicParameter: 'true'
+		}, {
 			requestId: "dvSecondaryReferenceIdentifier",
 			display: "Secondary Reference Time Series",
 			direction: "downchain",
 			publish: 'true',
 			period: 'Daily'
-		},{
+		}, {
 			requestId: "dvTertiaryReferenceIdentifier",
 			display: "Tertiary Reference Time Series",
 			direction: "downchain",
 			publish: 'true',
 			period: 'Daily'
-		},{
+		}, {
 			requestId: "dvQuaternaryReferenceIdentifier",
 			display: "Quaternary Reference Time Series",
 			direction: "downchain",
 			publish: 'true',
 			period: 'Daily'
-		}]
+		}],
 	
+	loadAllRequiredTimeseries: function () {
+		this.clearRatingModels();
+		
+		this.loadAllTimeSeriesOptions();
+		
+		if (this.model.get("selectedTimeSeries") && this.model.get("startDate") && this.model.get("endDate")) {
+			for (var i = 0; i < this.requiredRelatedTimeseriesConfig.length; i++) {
+				this.loadRelatedTimeseries(this.requiredRelatedTimeseriesConfig[i]);
+			}
+			for (var i = 0; i < this.optionalRelatedTimeseriesConfig.length; i++) {
+				this.loadRelatedTimeseries(this.optionalRelatedTimeseriesConfig[i]);
+			}
+		}
+	},
+	
+		
+	loadAllTimeSeriesOptions : function() {
+		for(var key in this.builtSelectorFields){
+			if(this.model.get("site")) {
+				if(this.selectorParams[key].dynamicParameter && this.model.get('selectedTimeSeries')){
+					this.selectorParams[key].parameter = this.model.get('selectedTimeSeries').parameter;
+				}
+				var tsSelector = this.builtSelectorFields[key];
+				var params = this.selectorParams[key];
+				this.populateTimeSeriesSelector(tsSelector, params);
+			} 
+		}
+	}
+
 });
