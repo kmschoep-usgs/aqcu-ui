6451689ee7f1d0c706104736afbccaad2b40072d
thongsav-usgs <thongsav@usgs.gov>
2015-12-28 10:20:10 -0600

    Added field visit retrieval (basic top level info) and using it for CORR report

--- src/main/webapp/js/view/reports/CorrectionsAtAGlanceReportView.js
+++ src/main/webapp/js/view/reports/CorrectionsAtAGlanceReportView.js
@@ -1,9 +1,4 @@
 AQCU.view.CorrectionsAtAGlanceReportView = AQCU.view.BaseReportView.extend({
 	reportName: "Corrections At A Glance",
-	reportType: "correctionsataglance",
-	optionalRatingModels: [{ 
-		requestId: "ratingModelIdentifier", 
-		display: "Primary Rating Model", 
-		bindTo: "primaryTimeseriesIdentifier"
-	}]
+	reportType: "correctionsataglance"
 });
