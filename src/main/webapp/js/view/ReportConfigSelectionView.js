f2b0fde2aef6eec24569ac4753dafa0367e7b462
kmschoep <kmschoep@IGSARMEWLTKMS.gs.doi.net>
2016-06-07 07:58:18 -0500

    Start work on show/hiding reports based on the selected time series.

--- src/main/webapp/js/view/ReportConfigSelectionView.js
+++ src/main/webapp/js/view/ReportConfigSelectionView.js
@@ -5,14 +5,14 @@ AQCU.view.ReportConfigSelectionView = AQCU.view.BaseView.extend({
 	
 	//Someday this may change dynamically depending on selected TS
 	availableReports: [
-		AQCU.view.CorrectionsAtAGlanceReportView,
-		AQCU.view.DvHydrographReportView,
-		AQCU.view.ExtremesReportView,
-		AQCU.view.FiveYearGWSummaryReportView,
-		AQCU.view.SensorReadingSummaryReportView,
-		AQCU.view.SiteVisitPeakReportView,
-		AQCU.view.UvHydrographReportView,
-		AQCU.view.VDiagramReportView
+	                   ['CORR', AQCU.view.CorrectionsAtAGlanceReportView],
+	                   ['DV', AQCU.view.DvHydrographReportView],
+	                   ['EXT', AQCU.view.ExtremesReportView],
+	                   ['5YR', AQCU.view.FiveYearGWSummaryReportView],
+	                   ['SRS', AQCU.view.SensorReadingSummaryReportView],
+	                   ['SVP', AQCU.view.SiteVisitPeakReportView],
+	                   ['UV', AQCU.view.UvHydrographReportView],
+	                   ['VD', AQCU.view.VDiagramReportView]
 		],
 	
 	/**
@@ -47,14 +47,19 @@ AQCU.view.ReportConfigSelectionView = AQCU.view.BaseView.extend({
 			
 	createReportViews: function() {
 		for (var i = 0; i < this.availableReports.length; i++) {
-			var view = new this.availableReports[i]({
-				parentModel: this.parentModel,
-				savedReportsController: this.savedReportsController,
-				selectedTimeSeries: this.selectedTimeSeries,
-				router: this.router
-			});
-			this.$('.available-reports').append(view.el);
-			this.availableReportViews.push(view);
+			if 	(
+				(_.contains(['CORR','EXT', 'SRS'], this.availableReports[i][0])) ||
+				(this.availableReports[i][0] == 'UV' && _.contains(['Instantaneous','Decumulated'],this.selectedTimeSeries.computation) && _.contains(['Points', 'Hourly'], this.selectedTimeSeries.period)) 
+				){
+				var view = new this.availableReports[i][1]({
+					parentModel: this.parentModel,
+					savedReportsController: this.savedReportsController,
+					selectedTimeSeries: this.selectedTimeSeries,
+					router: this.router
+				});
+				this.$('.available-reports').append(view.el);
+				this.availableReportViews.push(view);
+			}
 		}
 	},		
 			
