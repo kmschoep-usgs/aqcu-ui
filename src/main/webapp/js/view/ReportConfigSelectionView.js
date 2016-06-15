09b35cd402bb1ba41a8938f0aece04777f9b4692
kmschoep <kmschoep@IGSARMEWLTKMS.gs.doi.net>
2016-06-15 08:53:31 -0500

    Interim proof-of-concept commit for dynamic report buttons.
    Unique array of upchain and downchain processor types is populated and
    passed to ui.  the ui now inspects the selectedTimeSeries object to
    determine with report to show.  I'm concerned about race conditions,
    though, if the user tries to open another "folder" while another set of
    report buttons are still rendering.

--- src/main/webapp/js/view/ReportConfigSelectionView.js
+++ src/main/webapp/js/view/ReportConfigSelectionView.js
@@ -3,26 +3,40 @@
 AQCU.view.ReportConfigSelectionView = AQCU.view.BaseView.extend({
 	templateName: 'report-config-selection',
 	
-	//Someday this may change dynamically depending on selected TS
 	availableReports: [
-	                  // ['CORR', AQCU.view.CorrectionsAtAGlanceReportView],
-	                  // ['DV', AQCU.view.DvHydrographReportView],
-	                  // ['EXT', AQCU.view.ExtremesReportView],
-	                  // ['5YR', AQCU.view.FiveYearGWSummaryReportView],
-	                  // ['SRS', AQCU.view.SensorReadingSummaryReportView],
-	                  // ['SVP', AQCU.view.SiteVisitPeakReportView],
-	                  // ['UV', AQCU.view.UvHydrographReportView],
-	                  // ['VD', AQCU.view.VDiagramReportView]
-	                  AQCU.view.CorrectionsAtAGlanceReportView,
-	          		AQCU.view.DvHydrographReportView,
-	          		AQCU.view.ExtremesReportView,
-	          		AQCU.view.FiveYearGWSummaryReportView,
-	          		AQCU.view.SensorReadingSummaryReportView,
-	          		AQCU.view.SiteVisitPeakReportView,
-	          		AQCU.view.UvHydrographReportView,
-	          		AQCU.view.VDiagramReportView
+	                   ['CORR', AQCU.view.CorrectionsAtAGlanceReportView],
+	                   ['DV', AQCU.view.DvHydrographReportView],
+	                   ['EXT', AQCU.view.ExtremesReportView],
+	                   ['5YR', AQCU.view.FiveYearGWSummaryReportView],
+	                   ['SRS', AQCU.view.SensorReadingSummaryReportView],
+	                   ['SVP', AQCU.view.SiteVisitPeakReportView],
+	                   ['UV', AQCU.view.UvHydrographReportView],
+	                   ['VD', AQCU.view.VDiagramReportView]
 		],
-	
+		
+	// I don't love hard-coding these.  We'll see how much the list needs to change in the future...
+	gwReportParameters: [
+	                     "Elevation, GW, NGVD29", 
+	                     "Elevation, GW, NAVD88", 
+	                     "Elevation, GW, MSL",
+	                     "Elevation, GW, PRVD02",
+	                     "Elevation, GW, NMVD03",
+	                     "Elevation, GW, HILOCAL",
+	                     "Elevation, GW, GUVD63",
+	                     "Elevation, GW, GUVD04",
+	                     "Elevation, GW, ASVD62",
+	                     "Elevation, GW, ASVD02",
+	                     "Water level, depth LSD",
+	                     "Elevation, correct BP, NGVD29",
+	                     "Water level, depth MP",
+	                     "Elevation, water, inc, NGVD29",
+	                     "WaterLevel, BelowLSD"
+	                     ],
+
+	svpReportParameterLengthUnits: [
+	                               ".ft", 
+	                               ".in"],
+	            
 	/**
 	* Used by Backbone Stickit to bind HTML input elements to Backbone models.
 	* This will be built up in the initialize function.
@@ -86,41 +100,48 @@ AQCU.view.ReportConfigSelectionView = AQCU.view.BaseView.extend({
 		}
 	},
 	
-	//createReportViews: function() {
-		//this.fetchProcessorTypes(this.selectedTimeSeries);
-		//for (var i = 0; i < this.availableReports.length; i++) {
-		//	if 	(
-		//		(_.contains(['CORR','EXT', 'SRS'], this.availableReports[i][0])) ||
-		//		(this.availableReports[i][0] == 'UV' && _.contains(['Instantaneous','Decumulated'],this.selectedTimeSeries.computation) && _.contains(['Points', 'Hourly'], this.selectedTimeSeries.period)) 
-		//		){
-		//		var view = new this.availableReports[i][1]({
-		//			parentModel: this.parentModel,
-		//			savedReportsController: this.savedReportsController,
-		//			selectedTimeSeries: this.selectedTimeSeries,
-		//			router: this.router
-		//		});
-		//		this.$('.available-reports').append(view.el);
-		//		this.availableReportViews.push(view);
-		//	}
-		
-		
-		//}
-	//},		
-	
 	createReportViews: function() {
-		this.fetchProcessorTypes();
+		this.fetchProcessorTypes(this.selectedTimeSeries);
+		var _this = this;
 		for (var i = 0; i < this.availableReports.length; i++) {
-			var view = new this.availableReports[i]({
-				parentModel: this.parentModel,
-				savedReportsController: this.savedReportsController,
-				selectedTimeSeries: this.selectedTimeSeries,
-				router: this.router
-			});
-			this.$('.available-reports').append(view.el);
-			this.availableReportViews.push(view);
+			if 	(
+				(_.contains(['CORR','EXT', 'SRS'], this.availableReports[i][0])) 
+				|| (this.availableReports[i][0] == 'UV' 
+					&& _.contains(['Instantaneous','Decumulated'],this.selectedTimeSeries.computation) 
+					&& _.contains(['Points', 'Hourly'], this.selectedTimeSeries.period)) 
+				|| (this.availableReports[i][0] == 'DV'
+					&& (
+							_.contains(['Daily', 'Weekly'], this.selectedTimeSeries.period)  
+							|| (
+								_.contains(['Points', 'Hourly'], this.selectedTimeSeries.period) && _.contains(this.selectedTimeSeries.processorTypes.downChain, 'Statistics')
+							)
+						)
+					)
+				|| (this.availableReports[i][0] == '5YR' && _.contains(this.gwReportParameters, this.selectedTimeSeries.parameter)) 
+				|| (this.availableReports[i][0] == 'SVP' && _.some(this.svpReportParameterLengthUnits,function(unit){
+						return _this.selectedTimeSeries.identifier.indexOf(unit) > -1 
+							&& _this.selectedTimeSeries.identifier.indexOf(".ft^") == -1;
+						})
+					)
+				|| (this.availableReports[i][0] == 'VD' 
+					&& this.selectedTimeSeries.timeSeriesType === "ProcessorDerived" 
+					&& _.contains(this.selectedTimeSeries.processorTypes.upChain, 'RatingModel')
+					) 
+				){
+				var view = new this.availableReports[i][1]({
+					parentModel: this.parentModel,
+					savedReportsController: this.savedReportsController,
+					selectedTimeSeries: this.selectedTimeSeries,
+					router: this.router
+				});
+				this.$('.available-reports').append(view.el);
+				this.availableReportViews.push(view);
+			}
+		
+		
 		}
-	},
-			
+	},		
+	
 	removeReportViews: function() {		
 		while(this.availableReportViews.length > 0) {		
 			this.availableReportViews[0].remove();		
