61454d350ec78a9b172331a80623b9f2f41cc64f
zmoore-usgs <zmoore@usgs.gov>
2016-08-19 10:40:03 -0500

    AQCU-743: Limited correction exclusion UI to the CORR report.

--- src/main/webapp/js/view/reports/BaseReportView.js
+++ src/main/webapp/js/view/reports/BaseReportView.js
@@ -114,38 +114,7 @@ AQCU.view.BaseReportView = AQCU.view.BaseView.extend({
 			this.createRatingModelDisplay(this.optionalRatingModels[i], false);
 		}
                 
-                this.createCorrectionExclusionSelector();
-                this.bindToCorrectionExclusionSelectors(this.updateExcludedCorrections, this);
 	},
-        
-        //create exclude delete corrections filter
-	createCorrectionExclusionSelector: function() {
-		var excludeCorrectionField = $("<div><div><div class='row field-container'>" +
-				
-				"<div class='col-sm-5 col-md-5 col-lg-5'>" +
-				"<label for='excludedCorrections'>Exclude Corrections</label><br>" +
-				"</div>" +
-				
-				"<div class='checkbox col-sm-7 col-md-7 col-lg-7'>" +
-				"<label><input class='excludeDeleteRegion' name='excludeDeleteRegion' type='checkbox'>Exclude Delete Region Corrections</label>" +
-				"</div>" +
-				
-				"</div></div></div>");//not sure this warrants using a template YET
-		this.model.set("excludeDeleteRegion", false);
-		$.extend(this.bindings, {
-			".excludeDeleteRegion" : "excludeDeleteRegion"
-		});
-		this.advancedOptionsContainer.append(excludeCorrectionField);
-	},
-        
-        bindToCorrectionExclusionSelectors: function(bindFunc, scope) {
-		this.model.bind("change:excludeDeleteRegion", bindFunc, scope);
-	},
-        
-        updateExcludedCorrections: function() {
-                this.excludedCorrections = [];
-                this.excludedCorrections.push(this.model.get("excludeDeleteRegion")?"DELETE_REGION":null);
-        },
 	
 	createTimeseriesSelectionBox : function(params, isRequired) {
 		var container = this.advancedOptionsContainer;
