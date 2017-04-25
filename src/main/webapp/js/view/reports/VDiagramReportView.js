e55ae840da935e7fc56d48f85681adb25dcc66fc
zmoore-usgs <zmoore@usgs.gov>
2017-04-25 15:25:44 -0500

    AQCU-1064: Added a multiselect widget. Added the multiselect widget to the VDiagram popup window and added the data.

--- src/main/webapp/js/view/reports/VDiagramReportView.js
+++ src/main/webapp/js/view/reports/VDiagramReportView.js
@@ -17,6 +17,22 @@ AQCU.view.VDiagramReportView = AQCU.view.BaseReportView.extend({
 			required: true,
 			bindTo: "primaryTimeseriesIdentifier"
 		}],
+	controlConditions: [
+	    {text: "Unspecified", id: "unspecified"},
+	    {text: "Clear", id: "clear"},
+	    {text: "Fill Control Changed", id: "fillControlChanged"},
+	    {text: "Debris - Light", id: "debrisLight"},
+	    {text: "Debris - Moderate", id: "debrisModerate"},
+	    {text: "Debris - Heavy", id: "debrisHeavy"},
+	    {text: "Vegetation - Light", id: "vegetationLight"},
+	    {text: "Vegetation - Moderate", id: "vegetationModerate"},
+	    {text: "Vegetation - Heavy", id: "vegetationHeavy"},
+	    {text: "Ice - Anchor", id: "iceAnchor"},
+	    {text: "Ice - Cover", id: "iceCover"},
+	    {text: "Ice - Shore", id: "iceShore"},
+	    {text: "Submerged", id: "submerged"},
+	    {text: "No Flow", id: "noFlow"},
+	],
 	
 	removeSelectFields: function() {
 		if(this.priorYearsHistoric){
@@ -28,6 +44,7 @@ AQCU.view.VDiagramReportView = AQCU.view.BaseReportView.extend({
 	buildAdvancedOptions: function() {
 		AQCU.view.BaseReportView.prototype.buildAdvancedOptions.apply(this, arguments);
 		this.createHistoricSelector();
+		this.createControlConditionFilter();
 	},
 	
 	createHistoricSelector: function() {
@@ -45,24 +62,40 @@ AQCU.view.VDiagramReportView = AQCU.view.BaseReportView.extend({
 
 		this.priorYearsHistoric = new AQCU.view.SelectField({
 			el: '.aqcu-vdiag-history-years',
-			model : this.model,
+			model: this.model,
 			fieldConfig: {
 				fieldName   : "priorYearsHistoric",
 				displayName: "Years of Historic Measurements",
 				description : "This will set the number of years back from the selected data of historic measurements to include in the report"
 			},
 			renderTo: newContainer,
-			startHidden: false,
+			startHidden: false
 		});		
 		$.extend(this.bindings, this.priorYearsHistoric.getBindingConfig());
 		this.priorYearsHistoric.setSelectOptions(select2YearsBackData);
 	},
 	
+	createControlConditionFilter: function() {
+	    var newContainer = $('<div class="aqcu-vdiag-control-conditions"></div>');
+	    this.advancedOptionsContainer.append(newContainer);
+	    
+	    this.controlConditionFilter = new AQCU.view.MultiselectField({
+		el: '.aqcu-vdiag-control-conditions',
+		model : this.model,
+		fieldConfig: {
+			fieldName: "controlConditionFilter",
+			displayName: "Filter Control Condition",
+			description: "This will filter the control condition",
+			placeholder: "Filter..."
+		},
+		data: this.controlConditions
+	    });
+	},
 	
 	constructReportOptions: function() {
 		var reportOptions = AQCU.view.BaseReportView.prototype.constructReportOptions.apply(this, arguments);
 		if(this.model.get("priorYearsHistoric")) {
-			reportOptions.priorYearsHistoric = this.model.get("priorYearsHistoric");
+		    reportOptions.priorYearsHistoric = this.model.get("priorYearsHistoric");
 		}
  		return reportOptions;
 	}
---
+++ src/main/webapp/js/view/widgets/MultiselectField.js
@@ -0,0 +1,88 @@
+/**
+ * extend stickit to properly handle binding to select2 components
+ */
+Backbone.Stickit.addHandler([
+	{
+		selector: '.select2-hidden-accessible',
+		events: ['change'],
+		getVal: function($el) {
+			return $el.val();
+		},
+		update: function($el, val, model, options) {
+			$el.val(val);
+		}
+	}
+]);
+
+/**
+ * Backbone view to encapsulate the text field widget
+ */
+AQCU.view.MultiselectField = Backbone.View.extend({
+	/**
+	 * Handlebars template
+	 */
+	template: Handlebars.compile("\
+			<div class='row field-container'>\
+				{{#if displayName}}\
+				<div class='col-sm-5 col-md-5 col-lg-5'>\
+					<label for='{{fieldName}}'>{{displayName}}&nbsp;&nbsp;\
+					{{#if description}}\
+						<i class='fa fa-question-circle category-tip-target' title='{{description}}'></i>\
+					{{/if}}\
+					</label><br/>\
+				</div>\
+				{{/if}}\
+				<div class='{{#if displayName}}col-sm-7 col-md-7 col-lg-7{{else}}col-sm-12 col-md-12 col-lg-12{{/if}}'>\
+					<select class='aqcu_field aqcu_multi_select_field_{{fieldName}}'>\
+					</select>\
+				</div>\
+			</div>"),
+	/**
+	 * Backbone events object. See backbone documentation
+	 */
+	events: {
+	},
+	/**
+	 * Will create and render a multiselect field using a fieldConfig options
+	 */
+	initialize: function(options) {
+		// template configurations
+		this.fieldConfig = options.fieldConfig;
+		
+		// select2 configurations
+		this.select2 = {
+		    placeholder: this.fieldConfig.placeholder,
+		    width: '100%',
+		    data: options.data,
+		    multiple: true,
+		    minimumInputLength: -1,
+		    minimumResultsForSearch: Infinity
+		}
+		
+		this.selector    = ".aqcu_multi_select_field_" + options.fieldConfig.fieldName;
+		
+		// model bindings
+		this.model       = options.model;
+		this.modelField  = options.modelField ?options.modelField :options.fieldConfig.fieldName;
+		if (this.model.get(this.modelField) === undefined) {
+			this.model.set(this.modelField, '');
+		}
+		this.bindings = {}; // dynamic binding
+		this.bindings[this.selector] = this.modelField;
+		
+		this.render();
+	},
+	render: function() {
+		var newDom = this.template(this.fieldConfig); //new DOM elements created from templates
+		this.$el.html(newDom);
+		this.$(this.selector).select2(this.select2);
+		this.stickit();
+	},
+	getDisplayValue: function(value) {
+		// if no value is given then use the current value
+		if (value === undefined) {
+			value = $(this.selector).val();
+		}
+		return this.$(this.selector).find("option[value='"+value+"']").text();
+	}
+});
