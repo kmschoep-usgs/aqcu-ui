5a5a51289dc9031cd22c902e9d79003c69b30aae
Megan Hines <mhines@usgs.gov>
2017-05-09 09:54:13 -0500

    AQCU-1096: adding cog selection for primray rating model

--- src/main/webapp/js/view/reports/TimeSeriesSummaryFullReportView.js
+++ src/main/webapp/js/view/reports/TimeSeriesSummaryFullReportView.js
@@ -2,6 +2,12 @@ AQCU.view.TimeSeriesSummaryFullReportView = AQCU.view.BaseReportView.extend({
 	reportName: "Time Series Summary", 
 	reportAbbreviation: "TSS",
 	reportType: "timeseriessummary",
+	ratingModels: [{ 
+			requestId: "ratingModelIdentifier", 
+			display: "Primary Rating Model", 
+			required: false,
+			bindTo: "primaryTimeseriesIdentifier"
+	}],
 	excludedCorrections: [],
         
 	buildAdvancedOptions: function() {
