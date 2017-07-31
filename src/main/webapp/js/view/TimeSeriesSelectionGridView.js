4bee23e4c6e3e7f043f4c344f90290c7cba511cb
Megan Hines <mhines@usgs.gov>
2017-07-31 13:43:02 -0500

    AQCU-1163: add a binding for when dateSelection changes, to re-fetch the time series details in case the date affects the report availability

--- src/main/webapp/js/view/TimeSeriesSelectionGridView.js
+++ src/main/webapp/js/view/TimeSeriesSelectionGridView.js
@@ -35,6 +35,7 @@ AQCU.view.TimeSeriesSelectionGridView = AQCU.view.BaseView.extend({
 		this.model.bind("change:visibility",this.changeVisibility,this);
 		this.parentModel.bind("change:filter",this.displayGrid,this);
 		this.parentModel.bind("change:site", this.fetchTimeSeries, this);
+		this.parentModel.bind("change:dateSelection", this.fetchTimeSeries, this);
 		
 		this.displayedReportSelectors = {};
 		
